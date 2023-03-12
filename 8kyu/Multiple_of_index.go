090bfcdd6add527aa7eefb18254ee68ab7986d65
def <drystunn@gmail.com>
2023-03-12 13:55:47 +0300

    Solution

---
+++ 8kyu/Multiple_of_index.go
@@ -0,0 +1,11 @@
+package _kyu
+
+func multipleOfIndex(ints []int) []int {
+	res := []int{}
+	for i, num := range ints {
+		if (i != 0) && (num%i == 0) {
+			res = append(res, num)
+		}
+	}
+	return res
+}
