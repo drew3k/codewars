14c03e96f9acd7dddcea53fc379e906bb18f282f
def <drystunn@gmail.com>
2023-04-18 12:47:24 +0300

    added solution

---
+++ 7kyu/Band_name_generator.go
@@ -0,0 +1,11 @@
+package _kyu
+
+import "strings"
+
+func bandNameGenerator(word string) string {
+	if word[0] == word[len(word)-1] {
+		return strings.Title(word + word[1:])
+	} else {
+		return "The " + strings.Title(word)
+	}
+}
