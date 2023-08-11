01fb47f1afe36e4caca0c1b44a2247bdd4ca3cfc
def <drystunn@gmail.com>
2023-08-11 20:06:11 +0300

    fixed

--- 7kyu/Sum_of_integers_in_string.go
+++ 7kyu/Sum_of_integers_in_string.go
@@ -1,14 +1,12 @@
 package _kyu
 
 import (
-	"fmt"
 	"regexp"
 	"strconv"
 )
 
 func SumOfIntegersInString(strng string) int {
 	re := regexp.MustCompile("[0-9]+")
-	fmt.Println(re.FindAllString(strng, -1))
 	sum := 0
 	for _, i := range re.FindAllString(strng, -1) {
 		number, _ := strconv.Atoi(i)
